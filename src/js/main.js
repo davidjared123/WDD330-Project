f46eac9a2e4251455dacfbfd7311147d9d5c9c15
davidjared123 <davidjared123@gmail.com>
2026-06-06 21:10:38 -0400

    Option B: Newsletter sign up box on the homepage

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,17 @@
 import Alert from './Alert.js';
-import { loadHeaderFooter } from './utils.mjs';
+import { loadHeaderFooter, alertMessage } from './utils.mjs';
 
 loadHeaderFooter();
+
+const newsletterForm = document.getElementById('newsletter-form');
+if (newsletterForm) {
+  newsletterForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const emailInput = document.getElementById('newsletter-email');
+    alertMessage(`Thank you for subscribing with ${emailInput.value}!`, false);
+    newsletterForm.reset();
+  });
+}
 
 const alert = new Alert();
 alert.init();
